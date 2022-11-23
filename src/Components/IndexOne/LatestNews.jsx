e848f0de5a60d149fcb8127250471d97b9423d88
secret-point <secret.point2020@gmail.com>
2022-11-23 16:19:35 +0000

    news

--- src/Components/IndexOne/LatestNews.jsx
+++ src/Components/IndexOne/LatestNews.jsx
@@ -7,7 +7,7 @@ const LatestNews = () => {
       <div className="row">
         <div className="col-md-7">
           <div className="section-title mb-45">
-            <h2 className="title">Latest News Update <img src="assets/img/icons/title_icon01.png" alt="" /></h2>
+            <h2 className="title">Latest Updates <img src="assets/img/icons/title_icon01.png" alt="" /></h2>
           </div>
         </div>
       </div>
@@ -20,9 +20,9 @@ const LatestNews = () => {
             <div className="latest-news-content">
               <ul className="latest-news-meta">
                 <li><i className="flaticon-user" /><a href="/blog">Admin</a></li>
-                <li><i className="fi-sr-calendar" /> Jan 12, 2022</li>
+                <li><i className="fi-sr-calendar" /> Dec 12, 2022</li>
               </ul>
-              <h4 className="title"><a href="/blog-details">Top 5 Most Popular NFT Games world most powerful ?</a></h4>
+              <h4 className="title"><a href="/blog-details">Metacurio Presents : Top 5 Most Popular NFT Games.</a></h4>
               <a href="/blog-details" className="btn">read more</a>
             </div>
           </div>
@@ -35,7 +35,7 @@ const LatestNews = () => {
             <div className="latest-news-content">
               <ul className="latest-news-meta">
                 <li><i className="flaticon-user" /><a href="/blog">Admin</a></li>
-                <li><i className="fi-sr-calendar" /> Jan 19, 2022</li>
+                <li><i className="fi-sr-calendar" /> Dec 19, 2022</li>
               </ul>
               <h4 className="title"><a href="/blog-details">NFTs, rare digital items worlds crypto collectibles</a></h4>
               <a href="/blog-details" className="btn">read more</a>
@@ -50,9 +50,9 @@ const LatestNews = () => {
             <div className="latest-news-content">
               <ul className="latest-news-meta">
                 <li><i className="flaticon-user" /><a href="/blog">Admin</a></li>
-                <li><i className="fi-sr-calendar" /> Jan 19, 2022</li>
+                <li><i className="fi-sr-calendar" /> Dec 19, 2022</li>
               </ul>
-              <h4 className="title"><a href="/blog-details">Crypto enthusiasts on a single platform to create</a></h4>
+              <h4 className="title"><a href="/blog-details">Asian Market Tips And Tricks</a></h4>
               <a href="/blog-details" className="btn">read more</a>
             </div>
           </div>
